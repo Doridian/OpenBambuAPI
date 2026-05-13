6fbd95d7aaeb4740cb05331f274677bbe376bedb
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2026-05-13 20:16:40 +0200

    Update printer CA, add connect+publish+subscribe example (#58)

--- examples/mqtt.js
+++ examples/mqtt.js
@@ -2,6 +2,9 @@ const fs = require("fs");
 const path = require("path");
 const mqtt = require("mqtt");
 
+// Add the environment variables to your system or terminal
+// Run with `node --env-file=.env mqtt.js` or only `node mqtt.js`
+
 const bblCA = fs.readFileSync(path.join(__dirname, "ca_cert.pem"));
 
 function connectLocalMQTT(hostname, deviceID, accessCode) {
@@ -29,3 +32,36 @@ function connectCloudMQTT(username, accessToken) {
     password: accessToken,
   });
 }
+
+function onConnect() {
+  console.log("Connected to MQTT broker");
+  client.publish(
+    `device/${DEVICE_ID}/request`,
+    JSON.stringify({
+      system: {
+        sequence_id: 0,
+        command: "ledctrl",
+        led_node: "chamber_light",
+        led_mode: "on",
+      },
+    }),
+  );
+}
+
+function onMessage(topic, payload) {
+  console.log(topic, payload.toString());
+}
+
+const HOSTNAME = process.env.DEVICE_HOSTNAME;
+const DEVICE_ID = process.env.DEVICE_ID;
+const ACCESS_CODE = process.env.DEVICE_ACCESS_CODE;
+
+// const USERNAME = process.env.USERNAME;
+// const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+
+const client = connectLocalMQTT(HOSTNAME, DEVICE_ID, ACCESS_CODE);
+// const client = connectCloudMQTT(USERNAME, ACCESS_TOKEN);
+
+client.subscribe(`device/${DEVICE_ID}/report`);
+client.on("connect", onConnect);
+client.on("message", onMessage);
